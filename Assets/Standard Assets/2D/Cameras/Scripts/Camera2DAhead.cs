53f03e5a9323db86a68df027208c54153398db3b
Tiago <tiago_gobackto80s@hotmail.com>
2015-04-03 17:06:39 +0100

    Added some test animations, controllers and input. Added a camera that moves ahead of the player depending on his movement direction.

---
+++ Assets/Standard Assets/2D/Cameras/Scripts/Camera2DAhead.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera2DAhead : MonoBehaviour
+{
+    public Transform target;
+    public float damping = 0.1f;
+    public float lookAheadFactor = 3;
+    public float lookAheadMoveThreshold = 0.03f;
+    
+    private float _offsetZ;
+    private Vector3 _lastTargetPosition;
+    private float _currentVelocity;
+    private Vector3 _lookAheadPos;
+    
+    // Use this for initialization
+    private void Start()
+    {
+        _lastTargetPosition = target.position;
+        _offsetZ = (transform.position - target.position).z;
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // only update lookahead pos if accelerating or changed direction
+        float xMoveDelta = (target.position - _lastTargetPosition).x;
+        
+        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+        if (updateLookAheadTarget)
+        {
+            _lookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
+        }
+        
+        Vector3 aheadTargetPos = target.position + _lookAheadPos + Vector3.forward*_offsetZ;
+        aheadTargetPos.x = Mathf.SmoothDamp(transform.position.x, aheadTargetPos.x, ref _currentVelocity, damping);
+
+        transform.position = aheadTargetPos;
+        
+        _lastTargetPosition = target.position;
+    }
+}
---
+++ Assets/Standard Assets/2D/Scripts/MyRobotController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyRobotController : MonoBehaviour {
+
+	private bool _grounded = false;
+	private float _groundRadius = 0.2f;
+
+	private Animator _animator;
+	private Rigidbody2D _ridgidBody;
+
+	public float maxSpeed = 6.0f;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+    public float jumpForce = 700.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		_animator = GetComponent<Animator>();
+		_ridgidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate ()
+    {
+		_grounded = Physics2D.OverlapCircle(groundCheck.position, _groundRadius, whatIsGround);
+        _animator.SetBool("Grounded", _grounded);
+        _animator.SetFloat("VerticalSpeed", _ridgidBody.velocity.y);
+
+        if (!_grounded)
+        {
+            return;
+        }
+
+		var move = Input.GetAxis("Horizontal");
+
+		_animator.SetFloat("Speed", Mathf.Abs(move));
+
+		_ridgidBody.velocity = new Vector2(move * maxSpeed, _ridgidBody.velocity.y);
+
+		if (move > 0) {
+			Flip(true);
+		}
+		else if(move < 0){
+			Flip(false);
+		}
+	}
+
+    void Update()
+    {
+        if (_grounded && Input.GetButtonDown("Jump"))
+        {
+            _animator.SetBool("Grounded", false);
+            _ridgidBody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+	private void Flip(bool right)
+    {
+		var scale = transform.localScale;
+		if ((right && scale.x > 0) || (!right && scale.x < 0)) {
+			return;
+		}
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+   
+}
