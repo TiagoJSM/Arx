b7b20b0febc9427c9ba1b6c04bf294b61466d856
Tiago <tiago_gobackto80s@hotmail.com>
2015-04-11 00:04:41 +0100

    Added gizmos for waypoints and paths. Added data structure to fill with waypoints for platforms to move.

---
+++ Assets/Standard Assets/2D/Scripts/Waypoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDrawGizmos () 
+    {
+        DrawIcon();
+    }
+
+    void OnDrawGizmosSelected () 
+    {
+        DrawIcon();
+    }
+
+    private void DrawIcon()
+    {
+        Gizmos.DrawIcon (transform.position, "Waypoint.bmp");
+    }
+}
