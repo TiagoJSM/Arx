cbc9514b1448b5bbbdd5ea2fe6b2dc352328b438
TiagoMartins <tiago_gobackto80s@hotmail.com>
2017-01-24 19:52:52 +0000

    Added sparring dummy character.

---
+++ Arx/Assets/Standard Assets/Characters/Enemies/Sparring Dummy/Scripts/SparringDummyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Standard_Assets.Characters.Enemies.Sparring_Dummy.Scripts
+{
+    public class SparringDummyController : PlatformerCharacterController
+    {
+        protected override void Update()
+        {
+            base.Update();
+            StayStill();
+        }
+    }
+}
