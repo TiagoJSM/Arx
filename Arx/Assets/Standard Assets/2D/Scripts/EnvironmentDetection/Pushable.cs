48a21e259f2688b4c1268f85aba73a07f3f57d7e
TiagoMartins <tiago_gobackto80s@hotmail.com>
2017-02-18 01:14:31 +0000

    Fixed pushable object.

--- Arx/Assets/Standard Assets/2D/Scripts/EnvironmentDetection/Pushable.cs
+++ Arx/Assets/Standard Assets/2D/Scripts/EnvironmentDetection/Pushable.cs
@@ -6,25 +6,41 @@ using UnityEngine;
 
 namespace Assets.Standard_Assets._2D.Scripts.EnvironmentDetection
 {
+    [RequireComponent(typeof(CharacterController2D))]
     public class Pushable : MonoBehaviour
     {
+        private Vector3 _velocity;
         private float? _force;
 
+        private CharacterController2D _characterController2D;
+
         [SerializeField]
-        private Rigidbody2D _body;
+        private float _gravity = -25f;
+        [SerializeField]
+        private BoxCollider2D _collider;
 
         public void Push(float force)
         {
             _force = force;
         }
 
-        private void FixedUpdate()
+        private void Start()
         {
-            if(_force != null)
+            _characterController2D = GetComponent<CharacterController2D>();
+            _characterController2D.BoxCollider2D = _collider;
+        }
+
+        private void Update()
+        {
+            _velocity.x = 0;
+            _velocity.y += _gravity * Time.deltaTime;
+            if (_force != null)
             {
-                _body.AddForce(new Vector2(_force.Value, 0), ForceMode2D.Impulse);
+                _velocity.x = _force.Value;
             }
             _force = null;
+            _characterController2D.move(_velocity * Time.deltaTime);
+            _velocity = _characterController2D.velocity;
         }
     }
 }
