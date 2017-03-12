ba9e6a8a5ac0230b92c26fc47299381e69f928b4
TiagoMartins <tiago_gobackto80s@hotmail.com>
2017-03-12 22:38:35 +0000

    Added new scene. Added item id component and editor to keep track of collected items.

---
+++ Arx/Assets/Standard Assets/Environment/Pickup Item/Scripts/Editor/ItemIdEditorManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Standard_Assets.Environment.Pickup_Item.Scripts.Editor
+{
+    [InitializeOnLoad]
+    public class ItemIdEditorManager
+    {
+        static ItemIdEditorManager()
+        {
+            EditorApplication.hierarchyWindowChanged += VerifyItemId;
+        }
+
+        private static void VerifyItemId()
+        {
+
+        }
+    }
+}
---
+++ Arx/Assets/Standard Assets/Environment/Pickup Item/Scripts/ItemId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Standard_Assets.Environment.Pickup_Item.Scripts
+{
+    public class ItemId : MonoBehaviour
+    {
+        [SerializeField]
+        private Guid _id;
+        [SerializeField]
+        private string _sceneName;
+    }
+}
