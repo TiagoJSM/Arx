5c1875cefd67f5ced678f20eb2cd140e58dc9035
TiagoMartins <tiago_gobackto80s@hotmail.com>
2017-12-19 18:36:03 +0000

    Add heart component for life HUD.

---
+++ Arx/Assets/Standard Assets/UI/HUD/Scripts/HalfHeartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Standard_Assets.UI.HUD.Scripts
+{
+    [RequireComponent(typeof(Animator))]
+    public class HalfHeartController : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator _animator;
+
+        public void Lose()
+        {
+            _animator.SetTrigger("Lose");
+        }
+
+        public void Gain()
+        {
+            _animator.SetTrigger("Gain");
+        }
+    }
+}
