1033247c4e8d0fbbb0272daed279c0d34790d9bc
TiagoMartins <tiago_gobackto80s@hotmail.com>
2018-01-27 22:47:33 +0000

    Add trap tiles.

---
+++ Arx/Assets/Standard Assets/Environment/Hazards/Trap Tile/Scripts/TrapTile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Standard_Assets.Environment.Hazards.Trap_Tile.Scripts
+{
+    [RequireComponent(typeof(Animator))]
+    public class TrapTile : MonoBehaviour
+    {
+        private Animator _animator;
+        private bool _used;
+
+        [SerializeField]
+        private AudioSource _activationSound;
+        [SerializeField]
+        private Animator _hazardAnimator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!_used)
+            {
+                _used = true;
+                _animator.enabled = true;
+                _hazardAnimator.enabled = true;
+                _activationSound.Play();
+            }
+        }
+    }
+}
